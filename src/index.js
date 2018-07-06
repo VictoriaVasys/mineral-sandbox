b67b5abe31aa8864bcc0ed17ad75f3323a828396
Victoria Vasys <victoria.vasys@gmail.com>
2018-07-06 00:03:30 -0400

    Simple HTML form

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ThemeProvider } from "mineral-ui/themes";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
